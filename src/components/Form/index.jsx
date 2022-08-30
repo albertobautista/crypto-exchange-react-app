ea08c9d8a17e70c83c055c27e6962c6bafd1fa43
Alberto Bautista <alberto.bautista@capgemini.com>
2022-08-30 14:33:59 -0500

    Add Form component

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,4 +1,8 @@
 import styled from "@emotion/styled";
+import { useEffect, useState } from "react";
+import { currencies } from "../../data/currencies";
+import useSelectCurrencies from "../../hooks/useSelectCurrencies";
+import Error from "../Error";
 
 const InputSubmit = styled.input`
   background-color: #9497ff;
@@ -11,6 +15,7 @@ const InputSubmit = styled.input`
   font-size: 20px;
   border-radius: 5px;
   transition: background-color 0.3s ease;
+  margin-top: 30px;
 
   &:hover {
     background-color: #7a7dfe;
@@ -18,11 +23,57 @@ const InputSubmit = styled.input`
   }
 `;
 
-const Form = () => {
+const Form = ({ setCurrencies }) => {
+  const [cryptos, setCryptos] = useState([]);
+  const [error, setError] = useState(false);
+  const [currency, SelectCurrencies] = useSelectCurrencies(
+    "Selecciona tu moneda",
+    currencies
+  );
+  const [crypto, SelectCryptos] = useSelectCurrencies(
+    "Selecciona tu Criptomoneda",
+    cryptos
+  );
+  useEffect(() => {
+    const getCryptos = async () => {
+      const url =
+        "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
+
+      const response = await fetch(url);
+      const result = await response.json();
+
+      const arrayCryptos = result.Data.map((crypto) => ({
+        id: crypto.CoinInfo.Name,
+        name: crypto.CoinInfo.FullName,
+      }));
+      setCryptos(arrayCryptos);
+    };
+
+    getCryptos();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if ([currency, crypto].includes("")) {
+      setError(true);
+      return;
+    }
+    setError(false);
+    setCurrencies({
+      currency,
+      crypto,
+    });
+  };
   return (
-    <form>
-      <InputSubmit type="submit" value="Cotizar" />
-    </form>
+    <>
+      {error && <Error>Todos los campos son obligatorios</Error>}
+      <form onSubmit={handleSubmit}>
+        <SelectCurrencies />
+        <SelectCryptos />
+        <InputSubmit type="submit" value="Cotizar" />
+      </form>
+    </>
   );
 };
 
---
+++ src/data/currencies.js
@@ -0,0 +1,8 @@
+const currencies = [
+  { id: "USD", name: "USD dolar" },
+  { id: "MXN", name: "Peso Mexicano" },
+  { id: "EUR", name: "Euro" },
+  { id: "GBP", name: "Libra esterlina" },
+];
+
+export { currencies };
