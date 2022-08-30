daa5bb1cae73731caace1da86980c2e63c4c66e5
Alberto Bautista <alberto.bautista@capgemini.com>
2022-08-30 12:07:51 -0500

    Add Container, Image and Heading styled components

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import styled from "@emotion/styled";
+import CryptoImage from "./img/imagen-criptos.png";
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 1rem;
+  }
+`;
+
+const Image = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`;
+const Heading = styled.h1`
+  font-family: "Inconsolata", monospace;
+  color: #fff;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: "";
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`;
+
+function App() {
+  return (
+    <Container>
+      <Image src={CryptoImage} alt="Imágenes Criptomonedas" />
+      <div>
+        <Heading>Cotiza Criptomonedas al instante</Heading>;
+      </div>
+    </Container>
+  );
+}
+
+export default App;
